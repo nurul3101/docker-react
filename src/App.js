7a6d24719abd3061afabe34022937670bacac3da
Nurulraza Sundarani <nurulraza26@gmail.com>
2020-06-09 18:10:09 +0530

    Added link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CI /CD Dockerized React App Test</p>
+        <p>CI /CD Dockerized React App</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
